34f2ceaa0e46ccb5dfed5ea190778f38bfa5cee8
Carlos Macasaet <github@carlos-m.net>
2020-12-26 17:31:18 -0800

    Simplify and clean up solution for Day 6

--- src/test/java/com/macasaet/Day06.java
+++ src/test/java/com/macasaet/Day06.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class Day06 {
 
     public static void main(final String[] args) throws IOException {
-        try( var spliterator = new LineSpliterator( Day06.class.getResourceAsStream("/day-6-input.txt" ) ) ) {
+        try (var spliterator = new LineSpliterator(Day06.class.getResourceAsStream("/day-6-input.txt"))) {
             final var rawLines = StreamSupport.stream(spliterator, false)
                     .collect(Collectors.toUnmodifiableList());
             var currentGroup = new ArrayList<String>();
@@ -18,40 +19,27 @@ public class Day06 {
             final var groups = new ArrayList<List<String>>();
             for (final var line : rawLines) {
                 if (line.isBlank()) {
-                    if( currentGroup != null && !currentGroup.isEmpty() ) {
-                        groups.add( currentGroup );
+                    if (currentGroup != null && !currentGroup.isEmpty()) {
+                        groups.add(currentGroup);
                         currentGroup = new ArrayList<>();
                     }
                 } else {
-                    currentGroup.add( line );
+                    currentGroup.add(line);
                 }
             }
-            if( currentGroup != null && !currentGroup.isEmpty() ) {
-                groups.add( currentGroup );
+            if (currentGroup != null && !currentGroup.isEmpty()) {
+                groups.add(currentGroup);
             }
             final int sum = groups.stream().mapToInt(group -> {
-                final var uniqueCharacters = new HashSet<Integer>();
-                for( final var answers : group ) {
-                    answers.chars().forEach(answer -> {
-                        uniqueCharacters.add(answer);
-                    });
-                }
-                int total = 0;
-                for( final int c : uniqueCharacters ) {
-                    int numResponses = 0;
-                    for( final var answers : group ) {
-                        for( char a : answers.toCharArray() ) {
-                            if( (int)a == c ) {
-                                numResponses++;
-                                break;
-                            }
-                        }
-                        if( numResponses == group.size() ) {
-                            total++;
-                        }
-                    }
-                }
-                return total;
+                final var uniqueCharacters = group.stream()
+                        .flatMapToInt(String::chars)
+                        .collect(HashSet<Integer>::new, Set::add, Set::addAll);
+                return (int) uniqueCharacters.stream()
+                        .filter(c -> (int) group.stream()
+                            .map(String::chars)
+                            .filter(chars -> chars.anyMatch(answer -> answer == c))
+                            .count() == group.size())
+                        .count();
             }).sum();
             System.out.println(sum);
         }
