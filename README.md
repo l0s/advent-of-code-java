c6cad88a788d5a74acc164e27369db4eae5fdcb8
Carlos Macasaet <github@carlos-m.net>
2022-11-29 21:54:08 -0800

    Prepare for 2022

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Advent of Code 2021 (Java)
+# Advent of Code 2022 (Java)
 
 This is the code I used to solve the
-[Advent of Code](https://adventofcode.com/2021) puzzles. The main branch
+[Advent of Code](https://adventofcode.com/2022) puzzles. The main branch
 contains the most recent year in which I participated. Other years have
 their own branch.
 
 ## Other Editions
 
-* 2020 Advent of Code ([Java](https://github.com/l0s/advent-of-code-java/tree/2020) | [Rust](https://github.com/l0s/advent-of-code-rust))
+* 2020 Advent of Code ([Java](https://github.com/l0s/advent-of-code-java/tree/2020) | [Rust](https://github.com/l0s/advent-of-code-rust/releases/tag/y2020))
+* 2021 Advent of Code ([Java](https://github.com/l0s/advent-of-code-java/releases/tag/2021))
+* 2022 Advent of Code ([Rust](https://github.com/l0s/advent-of-code-rust))
--- src/test/java/com/macasaet/Day01.java
+++ src/test/java/com/macasaet/Day01.java
@@ -1,65 +1,43 @@
 package com.macasaet;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
-import org.junit.jupiter.api.Test;
-
 /**
- * --- Day 1: Sonar Sweep ---
+ * --- Day 1:  ---
  */
 public class Day01 {
 
     /**
-     * Perform a sonar sweep of the nearby sea floor.
      *
-     * @return measurements of the sea floor depth further and further away from the submarine
+     *
+     * @return
      */
-    protected List<Integer> getInput() {
+    protected List<String> getInput() {
         return StreamSupport
                 .stream(new LineSpliterator("day-01.txt"),
                         false)
-                .mapToInt(Integer::parseInt)
                 .collect(ArrayList::new, List::add, List::addAll);
     }
 
+    @Disabled
     @Test
     public final void part1() {
         final var list = getInput();
-        int increases = countIncreases(list);
-        System.out.println("Part 1: " + increases);
+
+        System.out.println("Part 1: " + null);
     }
 
+    @Disabled
     @Test
     public final void part2() {
         final var list = getInput();
-        final var windows = new LinkedList<Integer>();
-        for (int i = 2; i < list.size(); i++) {
-            windows.add(list.get(i) + list.get(i - 1) + list.get(i - 2));
-        }
-        final int increases = countIncreases(windows);
-        System.out.println("Part 2: " + increases);
-    }
 
-    /**
-     * Determine how quickly the depth increases.
-     *
-     * @param list progressively further measurements of the sea floor depth
-     * @return the number of times a depth measurement increase from the previous measurement
-     */
-    protected int countIncreases(final List<? extends Integer> list) {
-        int previous = list.get(0);
-        int increases = 0;
-        for (int i = 1; i < list.size(); i++) {
-            final var current = list.get(i);
-            if (current > previous) {
-                increases++;
-            }
-            previous = current;
-        }
-        return increases;
+        System.out.println("Part 2: " + null);
     }
 
 }
